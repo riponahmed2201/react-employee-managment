0276cd1bdd64e0a84ba16255d8ef4f53092d9aab
Ripon <riponahmed2201@gmail.com>
2022-06-09 12:29:01 +0600

    set project

---
+++ src/Page/Dashboard/index.js
@@ -0,0 +1,22 @@
+import React, {useState} from "react";
+import Swal from "sweetalert2";
+
+import Header from "./Header";
+import List from "./List";
+import Add from "./Add";
+import Edit from "./Edit";
+
+import { employeesData } from "../../data";
+
+function Dashboard() {
+
+    const [employees, setEmployees] = useState(employeesData);
+    const [selectedEmployee, setSelectedEmployee] = useState(null);
+    const [isAdding, setIsAdding] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+
+
+  return <div>Dashboard</div>;
+}
+
+export default Dashboard;
